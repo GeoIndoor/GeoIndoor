1355025bed84a9cdcafb55f2410333d947a8ed19
geodyj <whudyj2010@hotmail.com>
2019-08-22 12:01:27 +0800

    新建项目

---
+++ client/src/main/java/top/geomatics/ips/client/App.java
@@ -0,0 +1,13 @@
+package top.geomatics.ips.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
